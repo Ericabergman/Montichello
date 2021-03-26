4c02724ba5b50d98fcc4ee85423d6ee2266c1745
Ericabergman <erikabergman94@gmail.com>
2021-03-26 02:39:21 +0100

    add smooth scroll for navigation

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-$(".single-item").slick();
+// $(".single-item").slick();
 
-$(".header-container").slick();
+// $(".header-container").slick();
+
+var contry = "sweden";
+
+var secondConrty = "bulgarien";
+
+var theirdContry = "norge";
+contry + secondConrty;
+
+console.log(contry + secondConrty + theirdContry);
+
+var number = "100";
+var secondNumber = "200";
+
+console.log(number + secondNumber);
