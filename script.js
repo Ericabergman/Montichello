93f1cd0fae17338f409d8345fca485b5061a160c
Erica Bergman <70317153+Ericabergman@users.noreply.github.com>
2021-03-27 02:32:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// $(".single-item").slick();
 
-// $(".header-container").slick();
-
-var contry = "sweden";
-
-var secondConrty = "bulgarien";
-
-var theirdContry = "norge";
-contry + secondConrty;
-
-console.log(contry + secondConrty + theirdContry);
-
-var number = "100";
-var secondNumber = "200";
-
-console.log(number + secondNumber);
